fd6ee9344c1a4d12344f26d4ceb3c843f5655180
rychanya <rychanya@gmail.com>
2022-12-25 09:38:13 +0300

    💩

--- kittens_store/dependency.py
+++ kittens_store/dependency.py
@@ -1,12 +1,7 @@
 from fastapi import Request
 
-from kittens_store.bot import TG_App
 from kittens_store.store import BaseStore
 
 
 def store_depend(request: Request) -> BaseStore:
     return request.app.state.store
-
-
-def tg_depend(request: Request) -> TG_App:
-    return request.app.state.tg_app
