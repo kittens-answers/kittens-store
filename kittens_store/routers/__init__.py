ba7aff91564b07ff4a40a26022d32672eb41c937
rychanya <rychanya@gmail.com>
2022-12-25 09:56:19 +0300

    💩

--- kittens_store/routers/__init__.py
+++ kittens_store/routers/__init__.py
@@ -1,9 +1,8 @@
 from fastapi import FastAPI
 
-from . import questions, tg, tg_web_hook
+from . import tg, tg_web_hook
 
 
 def init_routers(app: FastAPI):
-    app.include_router(questions.router, prefix="/questions")
     app.include_router(tg.router, prefix="/tg")
     app.include_router(tg_web_hook.router, prefix="/tg")
